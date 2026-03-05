b9bfb31b31d51e28157527a578921ed86ab5180a
Skaro7 <marc_tau72@hotmail.com>
2026-03-05 18:29:03 +0100

    docs: add NoteMapper

---
+++ src/main/java/cli/agenda/tasks/notes/mapper/NoteMapper.java
@@ -0,0 +1,22 @@
+package cli.agenda.tasks.notes.mapper;
+
+import cli.agenda.tasks.notes.dto.NoteCreateDTO;
+import cli.agenda.tasks.notes.dto.NoteResponseDTO;
+import cli.agenda.tasks.notes.model.Note;
+
+public class NoteMapper {
+
+    private NoteMapper() {}
+
+    public static Note toNote(NoteCreateDTO dto) {
+        return new Note(dto.getTitle(), dto.getContent());
+    }
+
+    public static NoteResponseDTO toResponseDTO(Note note) {
+        return new NoteResponseDTO(
+                note.getId().toString(),
+                note.getTitle(),
+                note.getContent()
+        );
+    }
+}
