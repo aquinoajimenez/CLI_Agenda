20cd5ac2108af6d5e72e32a696d10f0f19b259fd
Skaro7 <marc_tau72@hotmail.com>
2026-03-08 17:10:42 +0100

    docs: rebase notes out of tasks
    # Conflicts:
    #	src/main/java/cli/agenda/notes/dto/NoteCreateDTO.java
    #	src/main/java/cli/agenda/notes/dto/NoteResponseDTO.java
    #	src/main/java/cli/agenda/notes/mapper/NoteMapper.java
    #	src/main/java/cli/agenda/notes/model/Note.java

---
+++ src/main/java/cli/agenda/notes/dto/NoteResponseDTO.java
@@ -0,0 +1,18 @@
+package cli.agenda.notes.dto;
+
+public class NoteResponseDTO {
+
+    private String id;
+    private String title;
+    private String content;
+
+    public NoteResponseDTO(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getContent() { return content; }
+}
---
+++ src/main/java/cli/agenda/notes/model/Note.java
@@ -0,0 +1,38 @@
+package cli.agenda.notes.model;
+
+import org.bson.types.ObjectId;
+
+public class Note {
+    private ObjectId id;
+    private String title;
+    private String content;
+
+public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+}
+
+public Object getId() {
+        return id;
+    }
+
+public void setId(ObjectId id) {
+        this.id = id;
+}
+
+public String getTitle() {
+        return title;
+    }
+
+public void setTitle(String title) {
+        this.title = title;
+    }
+
+public String getContent() {
+        return content;
+    }
+
+public void setContent(String content) {
+        this.content = content;
+    }
+}
