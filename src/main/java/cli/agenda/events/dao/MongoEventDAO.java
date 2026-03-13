daf37dd2f601c3751ecce492aa22b57857b709fb
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2026-03-13 15:33:03 +0100

    feat: dependencies added in pom, added methods in eventdao interface, added methods not finished in mongoeventdao and class eventdaotest created

--- src/main/java/cli/agenda/events/dao/MongoEventDAO.java
+++ src/main/java/cli/agenda/events/dao/MongoEventDAO.java
@@ -1,8 +1,9 @@
 package cli.agenda.events.dao;
 
 import com.mongodb.client.MongoCollection;
-
 import org.bson.Document;
+import java.util.List;
+import java.util.Optional;
 
 public class MongoEventDAO implements EventDao{
     private final MongoCollection<Document> collection;
@@ -15,5 +16,30 @@ public class MongoEventDAO implements EventDao{
     public String save(Document document) {
         collection.insertOne(document);
         return document.getObjectId("_id").toHexString();
+    }
+
+    @Override
+    public boolean update(String id, Document updates) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public Optional<Document> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Document> findUpComing() {
+        return List.of();
+    }
+
+    @Override
+    public List<Document> findPast() {
+        return List.of();
     }
 }
