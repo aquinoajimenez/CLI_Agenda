67a18f2ab471aac6ea14ea8ec852cada92e4b84e
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2026-03-13 23:46:18 +0100

    feat: dao completed using factory pattern

--- src/main/java/cli/agenda/events/dao/EventDaoFactory.java
+++ src/main/java/cli/agenda/events/dao/EventDaoFactory.java
@@ -5,7 +5,6 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
 public class EventDaoFactory {
-
     public static EventDao createMongoEventDao(){
         MongoCollection<Document> collection = DatabaseConnection.INSTANCE.getDatabase().getCollection("events");
         return new MongoEventDAO(collection);
--- src/main/java/cli/agenda/events/dao/MongoEventDAO.java
+++ src/main/java/cli/agenda/events/dao/MongoEventDAO.java
@@ -1,7 +1,14 @@
 package cli.agenda.events.dao;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,26 +27,45 @@ public class MongoEventDAO implements EventDao{
 
     @Override
     public boolean update(String id, Document updates) {
-        return false;
+        try{
+            Document updateQuery = new Document("$set", updates);
+            UpdateResult result = collection.updateOne(Filters.eq("_id", new ObjectId(id)), updateQuery);
+            return result.getModifiedCount() > 0;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        try{
+            DeleteResult delete = collection.deleteOne(Filters.eq("_id", new ObjectId(id)));
+            return delete.getDeletedCount() > 0;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 
     @Override
     public Optional<Document> findById(String id) {
-        return Optional.empty();
+        try{
+            Document findId = collection.find(Filters.eq("_id", new ObjectId(id))).first();
+            return Optional.ofNullable(findId);
+        }catch(IllegalArgumentException e){
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Document> findUpComing() {
-        return List.of();
+        Date date = new Date();
+
+        return collection.find(Filters.gt("end_date", date)).into(new ArrayList<>());
     }
 
     @Override
     public List<Document> findPast() {
-        return List.of();
+        Date date = new Date();
+        return collection.find(Filters.lt("end_date", date)).into(new ArrayList<>());
     }
 }
