22858fa5c0ecd15a67157d5a4480908e7b86616c
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2026-03-15 15:14:54 +0100

    feat: added in pom mockito dependency, added in eventmapper copuwithid function, repository completed and tests completed

--- src/main/java/cli/agenda/events/repository/EventRepository.java
+++ src/main/java/cli/agenda/events/repository/EventRepository.java
@@ -1,4 +1,16 @@
 package cli.agenda.events.repository;
 
+import cli.agenda.events.model.Event;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EventRepository {
+    Event save(Event event);
+    boolean update(String id, Event event);
+    boolean delete(String id);
+
+    Optional<Event> findById(String id);
+    List<Event> findUpComing();
+    List<Event> findPast();
 }
